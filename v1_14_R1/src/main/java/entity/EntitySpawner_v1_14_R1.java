902fe6df393fccf3833f4e09aceccb48f635cd51
inventivetalent <mail@inventivetalent.org>
2019-08-22 18:47:11 +0200

    add v1_14_R1 module

---
+++ v1_14_R1/src/main/java/entity/EntitySpawner_v1_14_R1.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015-2016 inventivetalent. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are
+ *  permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *        of conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  The views and conclusions contained in the software and documentation are those of the
+ *  authors and contributors and should not be interpreted as representing official policies,
+ *  either expressed or implied, of anybody else.
+ */
+
+package entity;
+
+import net.minecraft.server.v1_14_R1.Block;
+import net.minecraft.server.v1_14_R1.IBlockData;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_14_R1.block.data.CraftBlockData;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.inventivetalent.advancedslabs.entity.IEntitySpawner;
+import org.inventivetalent.advancedslabs.entity.ISlabFallingBlock;
+
+public class EntitySpawner_v1_14_R1 implements IEntitySpawner {
+
+	@Override
+	public ISlabFallingBlock spawnFallingBlock(Location location, Material material, String data) {
+		// From CraftBlockData
+
+		IBlockData blockData = CraftBlockData.newData(material,data).getState();
+
+		SlabEntityFallingSand_v1_14_R1 entity = new SlabEntityFallingSand_v1_14_R1(((CraftWorld) location.getWorld()).getHandle(), location.getX(), location.getY(), location.getZ(), blockData);
+		((CraftWorld) location.getWorld()).getHandle().addEntity(entity, CreatureSpawnEvent.SpawnReason.CUSTOM);
+		return entity;
+	}
+
+	public ISlabFallingBlock spawnFallingBlock(Location location, Material material, byte data) {
+		///TODO: find a way to still support IDs/data properly
+		SlabEntityFallingSand_v1_14_R1 entity = new SlabEntityFallingSand_v1_14_R1(((CraftWorld) location.getWorld()).getHandle(), location.getX(), location.getY(), location.getZ(), Block.getByCombinedId(material.getId()));
+		((CraftWorld) location.getWorld()).getHandle().addEntity(entity, CreatureSpawnEvent.SpawnReason.CUSTOM);
+		return entity;
+	}
+
+}
